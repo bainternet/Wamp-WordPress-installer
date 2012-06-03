5aca3b63791b6134e76bf106713d661c055db17e
bainternet <admin@bainternet.info>
2012-06-03 22:17:18 +0300

    see readme.md

--- installer.php
+++ installer.php
@@ -1,31 +1,77 @@
 <?php
 /**
-* Simple WordPress Auto Installer for WAMP
+* Simple WordPress Auto Installer 
 *
-* 
+* @author Ohad Raz <admin@bainternet.info> | http://en.bainternet.info
+* @package WordPress Auto Installer
+* @version 0.2
 * @file: installer.php
-* @author Ohad Raz <admin@bainternet.info>
-* @version 0.1
+* @todo Better error handling.
 *  
 */
 ?>
 <?php
-//heb version
-define('HEB_LINK','http://he.wordpress.org/wordpress-3.3.1-he_IL.zip');
-//beta versions
-define('LAST_NIGHTLY' , "http://wordpress.org/nightly-builds/wordpress-latest.zip");
+$last_v_doted = '3.3.2';
+$versions = array(
+  array(
+    'label' => 'Local file', 
+    'url' => 'local',
+    'description' => 'Use a custom install , zip to a file named latest.zip and place in the same directory of this file.'
+  ),
+  array(
+    'label' => 'Latest version', 
+    'url' => 'http://wordpress.org/latest.zip',
+    'description' => 'Download the latest stable English version'
+  ),
+  array(
+    'label' => 'Last nightly', 
+    'url' => 'http://wordpress.org/nightly-builds/wordpress-latest.zip',
+    'description' => 'Download the latest beta version'
+  ),
+  array(
+    'label' => 'Hebrew', 
+    'url' => 'http://he.wordpress.org/wordpress-'.$last_v_doted.'-he_IL.zip',
+    'description' => 'Download the latest stable Hebrew version'
+  ),
+  array(
+    'label' => 'Italian', 
+    'url' => 'http://it.wordpress.org/wordpress-'.$last_v_doted.'-it_IT.zip',
+    'description' => 'Download the latest stable Italian version'
+  ),
+  array(
+    'label' => 'French', 
+    'url' => 'http://fr.wordpress.org/wordpress-'.$last_v_doted.'-fr_FR.zip',
+    'description' => 'Download the latest stable French version'
+  ),
+  array(
+    'label' => 'Spanish', 
+    'url' => 'http://es.wordpress.org/wordpress-'.$last_v_doted.'-es_ES.zip',
+    'description' => 'Download the latest stable Spanish version'
+  ),
+  array(
+    'label' => 'Romanian', 
+    'url' => 'http://ro.wordpress.org/wordpress-'.$last_v_doted.'-ro_RO.zip',
+    'description' => 'Download the latest stable Romanian version'
+  ),
+  array(
+    'label' => 'Russian', 
+    'url' => 'http://ru.wordpress.org/wordpress-'.$last_v_doted.'-ru_RU.zip',
+    'description' => 'Download the latest stable Russian version'
+  ),
+  array(
+    'label' => 'Japanese', 
+    'url' => 'http://ja.wordpress.org/wordpress-'.$last_v_doted.'-ja.zip',
+    'description' => 'Download the latest stable Japanese  version'
+  ),
+  //any other language you want goes here
+);
 
 $download_ok  = false;
 $done_zip = false;
 if (isset($_POST['install'])){
   //download latest
-  if (isset($_POST['file_location']) && $_POST['file_location'] == 'remote'){
-    if (isset($_POST['lang']) && $_POST['lang'] == "Heb")
-      $download_ok = http_get_file(HEB_LINK,'latest.zip');
-    elseif (isset($_POST['lang']) && $_POST['lang'] == "NIGHTLY")
-      $download_ok = http_get_file(LAST_NIGHTLY,'latest.zip');
-    else
-      $download_ok = http_get_file("http://wordpress.org/latest.zip",'latest.zip');
+  if (isset($_POST['ver']) && $_POST['ver'] != 'local'){
+    $download_ok = http_get_file($versions[$_POST['ver']]['url'],'latest.zip');
   }else{
     //use local file
     $download_ok = file_exists('latest.zip');
@@ -49,7 +95,7 @@ if (isset($_POST['install'])){
   if ($done_zip){
     ?>
     <div style="display: none;">
-      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
+      <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
       <form action="<?php echo $_POST['folder'].'/wp-admin/setup-config.php?step=2'; ?>" method="post" id="insa">
       <p>Below you should enter your database connection details. If you're not sure about these, contact your host. </p>
       <table class="form-table">
@@ -75,7 +121,7 @@ if (isset($_POST['install'])){
         </tr>
         <tr>
           <th scope="row"><label for="prefix">Table Prefix</label></th>
-          <td><input type="text" size="25" value="wp_" id="prefix" name="prefix"></td>
+          <td><input type="text" size="25" value="<?php echo $_POST['db_prefix'];?>" id="prefix" name="prefix"></td>
           <td>If you want to run multiple WordPress installations in a single database, change this.</td>
         </tr>
       </tbody></table>
@@ -92,59 +138,104 @@ if (isset($_POST['install'])){
   }
 }else{
   ?>
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
   <html>
   <head>
     <title>WordPress Installer For WAMP</title>
-    <style>
-    html{background:#f9f9f9;}body{background:#fff;color:#333;font-family:sans-serif;margin:2em auto;padding:1em 2em;-webkit-border-radius:3px;border-radius:3px;border:1px solid #dfdfdf;max-width:700px;}a{color:#21759B;text-decoration:none;}a:hover{color:#D54E21;}h1{border-bottom:1px solid #dadada;clear:both;color:#666;font:24px Georgia,"Times New Roman",Times,serif;margin:30px 0 0 0;padding:0;padding-bottom:7px;}h2{font-size:16px;}p,li,dd,dt{padding-bottom:2px;font-size:14px;line-height:1.5;}code,.code{font-size:14px;}ul,ol,dl{padding:5px 5px 5px 22px;}a img{border:0;}abbr{border:0;font-variant:normal;}#logo{margin:6px 0 14px 0;border-bottom:none;text-align:center;}.step{margin:20px 0 15px;}.step,th{text-align:left;padding:0;}.submit input,.button,.button-secondary{font-family:sans-serif;text-decoration:none;font-size:14px!important;line-height:16px;padding:6px 12px;cursor:pointer;border:1px solid #bbb;color:#464646;-webkit-border-radius:15px;border-radius:15px;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box;}.button:hover,.button-secondary:hover,.submit input:hover{color:#000;border-color:#666;}.button,.submit input,.button-secondary{background:#f2f2f2 url(../images/white-grad.png) repeat-x scroll left top;}.button:active,.submit input:active,.button-secondary:active{background:#eee url(../images/white-grad-active.png) repeat-x scroll left top;}textarea{border:1px solid #dfdfdf;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;width:695px;}.form-table{border-collapse:collapse;margin-top:1em;width:100%;}.form-table td{margin-bottom:9px;padding:10px 20px 10px 0;border-bottom:8px solid #fff;font-size:14px;vertical-align:top;}.form-table th{font-size:14px;text-align:left;padding:16px 20px 10px 0;border-bottom:8px solid #fff;width:140px;vertical-align:top;}.form-table code{line-height:18px;font-size:14px;}.form-table p{margin:4px 0 0 0;font-size:11px;}.form-table input{line-height:20px;font-size:15px;padding:2px;border:1px #DFDFDF solid;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;}.form-table input[type=text],.form-table input[type=password]{width:206px;}.form-table th p{font-weight:normal;}.form-table.install-success td{vertical-align:middle;padding:16px 20px 10px 0;}.form-table.install-success td p{margin:0;font-size:14px;}.form-table.install-success td code{margin:0;font-size:18px;}#error-page{margin-top:50px;}#error-page p{font-size:14px;line-height:18px;margin:25px 0 20px;}#error-page code,.code{font-family:Consolas,Monaco,monospace;}#pass-strength-result{background-color:#eee;border-color:#ddd!important;border-style:solid;border-width:1px;margin:5px 5px 5px 0;padding:5px;text-align:center;width:200px;display:none;}#pass-strength-result.bad{background-color:#ffb78c;border-color:#ff853c!important;}#pass-strength-result.good{background-color:#ffec8b;border-color:#fc0!important;}#pass-strength-result.short{background-color:#ffa0a0;border-color:#f04040!important;}#pass-strength-result.strong{background-color:#c3ff88;border-color:#8dff1c!important;}.message{border:1px solid #e6db55;padding:.3em .6em;margin:5px 0 15px;background-color:#ffffe0;}body.rtl{font-family:Tahoma,arial;}.rtl h1{font-family:arial;margin:5px -4px 0 0;}.rtl ul,.rtl ol{padding:5px 22px 5px 5px;}.rtl .step,.rtl th,.rtl .form-table th{text-align:right;}.rtl .submit input,.rtl .button,.rtl .button-secondary{margin-right:0;}.rtl #user_login,.rtl #admin_email,.rtl #pass1,.rtl #pass2{direction:ltr;}
+    <style type="text/css">
+    html{background:#f9f9f9;}body{background:#fff;color:#333;font-family:sans-serif;margin:2em auto;padding:1em 2em;-webkit-border-radius:3px;border-radius:3px;border:1px solid #dfdfdf;max-width:700px;}a{color:#21759B;text-decoration:none;}a:hover{color:#D54E21;}h1{border-bottom:1px solid #dadada;clear:both;color:#666;font:24px Georgia,"Times New Roman",Times,serif;margin:30px 0 0 0;padding:0;padding-bottom:7px;}h2{font-size:16px;}p,li,dd,dt{padding-bottom:2px;font-size:14px;line-height:1.5;}code,.code{font-size:14px;}ul,ol,dl{padding:5px 5px 5px 22px;}a img{border:0;}abbr{border:0;font-variant:normal;}#logo{margin:6px 0 14px 0;border-bottom:none;text-align:center;}.step{margin:20px 0 15px;}.step,th{text-align:left;padding:0;}.submit input,.button,.button-secondary{font-family:sans-serif;text-decoration:none;font-size:14px!important;line-height:16px;padding:6px 12px;cursor:pointer;border:1px solid #bbb;color:#464646;-webkit-border-radius:15px;border-radius:15px;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box;}.button:hover,.button-secondary:hover,.submit input:hover{color:#000;border-color:#666;}.button,.submit input,.button-secondary{background:#f2f2f2 url(../images/white-grad.png) repeat-x scroll left top;}.button:active,.submit input:active,.button-secondary:active{background:#eee url(../images/white-grad-active.png) repeat-x scroll left top;}textarea{border:1px solid #dfdfdf;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;width:695px;}.form-table{border-collapse:collapse;margin-top:1em;width:100%;}.form-table td{margin-bottom:9px;padding:10px 20px 10px 0;border-bottom:8px solid #fff;font-size:14px;vertical-align:top;}.form-table th{font-size:14px;text-align:left;padding:16px 20px 10px 0;border-bottom:8px solid #fff;width:140px;vertical-align:top;}.form-table code{line-height:18px;font-size:14px;}.form-table p{margin:4px 0 0 0;font-size:11px;}.form-table input{line-height:20px;font-size:15px;padding:2px;border:1px #DFDFDF solid;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;}.form-table input[type=text],.form-table input[type=password]{width:206px;}.form-table th p{font-weight:normal;}.form-table.install-success td{vertical-align:middle;padding:16px 20px 10px 0;}.form-table.install-success td p{margin:0;font-size:14px;}.form-table.install-success td code{margin:0;font-size:18px;}#error-page{margin-top:50px;}#error-page p{font-size:14px;line-height:18px;margin:25px 0 20px;}#error-page code,.code{font-family:Consolas,Monaco,monospace;}#pass-strength-result{background-color:#eee;border-color:#ddd!important;border-style:solid;border-width:1px;margin:5px 5px 5px 0;padding:5px;text-align:center;width:200px;display:none;}#pass-strength-result.bad{background-color:#ffb78c;border-color:#ff853c!important;}#pass-strength-result.good{background-color:#ffec8b;border-color:#fc0!important;}#pass-strength-result.short{background-color:#ffa0a0;border-color:#f04040!important;}#pass-strength-result.strong{background-color:#c3ff88;border-color:#8dff1c!important;}.message{border:1px solid #e6db55;padding:.3em .6em;margin:5px 0 15px;background-color:#ffffe0;}body.rtl{font-family:Tahoma,arial;}.rtl h1{font-family:arial;margin:5px -4px 0 0;}.rtl ul,.rtl ol{padding:5px 22px 5px 5px;}.rtl .step,.rtl th,.rtl .form-table th{text-align:right;}.rtl .submit input,.rtl .button,.rtl .button-secondary{margin-right:0;}.rtl #user_login,.rtl #admin_email,.rtl #pass1,.rtl #pass2{direction:ltr;}#description {background-repeat: no-repeat;color: #4C4C4C;font-family: Georgia,"Times New Roman",Times,serif;font-size: 14px;font-style: italic;min-height: 45px;padding: 15px 10px;background: none repeat scroll 0 0 #DBECF8;border-radius: 6px 6px 6px 6px;float: right;}
     </style>
   </head>
   <body>
     <div>
-      <h1 id="logo"> <img src="http://s.wordpress.org/about/images/logos/wordpress-logo-hoz-rgb.png"> </h1>
-      <p>Simple WordPress Auto Installer for  WAMP</p>
-      <p>By <a href="http://en.bainternet.info">Bainternet WordPress Developer</a></p>
-  <form method="POST" name="installer_form">
+      <h1 id="logo">
+    <img src="http://s.wordpress.org/about/images/logos/wordpress-logo-hoz-rgb.png">
+    </h1>
+      <p>Simple WordPress Auto Installer for WAMP ( XAMP and other stacks)</p>
+      <p>By Bainternet <a href="http://en.bainternet.info">WordPress Developer</a></p>
+      <form method="POST" name="installer_form">
     <p>
       <input name="installer" type="hidden" value="ins" /> 
-      <input checked="checked" name="file_location" type="checkbox" value="remote" /> Download latest version
-      <input name="lang" type="radio" value="Heb" /> Hebrew Install
-      <input name="lang" type="radio" value="NIGHTLY" /> Last Nightly built
+    Select Your installation:
+    <select name="ver" id="ver">
+    <?php
+      foreach ($versions as $key => $arr){
+        $radios .=  '<option  value="'.$key.'" />'.$arr['label'].'</option>';
+      }
+      $radios  = str_replace('value="1"','value="1" selected="selected"',$radios);
+      echo $radios;
+    ?>
+    </select><br/>
+    <span id="description">
+
+    </span>
     </p>
     <p>Folder to create:  
-      <input name="folder" type="input" value="" />
+      <input name="folder" id="folder" type="text" value="" />
     </p>
     <p>
-      <input  name="mk_db" type="checkbox" value="mkdb" /> Create Database?
+      <input  name="mk_db" type="checkbox" value="mkdb" checked="checked"/> Create Database?
     </p>
     <p>Database server (localhost?)
-      <input name="server" type="input" value="localhost" />
+      <input name="server" type="text" value="localhost" />
     </p>
     <p>Database Name
-      <input name="db_name" type="input" value="" />
+      <input name="db_name" id="db_name" type="text" value="" />
     </p>
     <p>Database User Name
-      <input name="db_user" type="input" value="" />
+      <input name="db_user" type="text" value="" />
     </p>
     <p>Database User password
-      <input name="db_pass" type="input" value="" />
+      <input name="db_pass" type="text" value="" />
+    </p>
+    <p>Table Prefix
+      <input name="db_prefix" type="text" value="wp_" />
     </p>
     <p class="step">
       <input name="install" type="submit" class="button" value="GO" />
     </p>
   </form>
+  <div style="text-align: center">
+    <small>
+      <a href="hhttps://github.com/bainternet/Wamp-WordPress-installer/zipball/master">
+        Download
+      </a> |
+      <a href="https://github.com/bainternet/Wamp-WordPress-installer/issues">
+        Bugs and Support
+      </a> |
+      <a href="http://en.bainternet.info" title="My other WordPress Goodies">
+        More WordPress Goodies
+      </a>      
+    </small>
   </div>
-
+  </div>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script>
+  var descs = [];
+  jQuery(document).ready(function(){
+    <?php
+    foreach ($versions as $key => $arr){
+      echo  'descs[' . $key . '] = "' .$arr['description']. '";';
+      echo "\n";
+    }
+    ?>
+    jQuery("#folder").change(function(){
+      jQuery("#db_name").val(jQuery("#folder").val());
+    });
+    jQuery("#description").html(descs[jQuery("#ver").val()]);
+    jQuery("#ver").change(function(){
+      jQuery("#description").hide('fast');
+      jQuery("#description").html(descs[jQuery(this).val()]);
+      jQuery("#description").show('2300');
+    });
+  });
+  </script>
+  </body>
+  </html>
   <?php
 }
 
 
-
-
-
-
-
-//download remote file
 /**
  * http_get_file simple function to download a file using curl
  * 
@@ -171,9 +262,9 @@ function http_get_file($remote_url, $local_file){
  * 
  * @author Ohad Raz <admin@bainternet.info>
  * @since 0.1
- * 
  * @param  (string) $zip_File zip file name (full path)
  * @param  (string) $folder   destenation folder full path
+ * 
  * @return (bool)
  */
 function extract_zip($zip_File,$folder){
@@ -195,11 +286,11 @@ function extract_zip($zip_File,$folder){
  * 
  * @author Ohad Raz <admin@bainternet.info>
  * @since 0.1
- * 
  * @param  (string) $user    mysql user name
  * @param  (string) $pass    mysql user password
  * @param  (string) $server  server name
  * @param  (string) $db_name database name to create
+ * 
  * @return (bool)
  */
 function create_db($user,$pass,$server,$db_name){
--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
 # Simple WordPress Auto Installer for WAMP
-![wamp wordpress installer](http://i.imgur.com/xckvR.png)
+![wamp wordpress installer](http://i.imgur.com/cvZZK.jpg)
 ## **Background**
-Simple WordPress Auto Installer for [WAMP](http://www.wampserver.com/en/)(and maybe other amp stacks like xampp....) is something that i coded after a friend of mine asked me if there is a fantastico installer for WAMP, which was funny since he only needed it to install WordPress. 
+Simple WordPress Auto Installer for [WAMP](http://www.wampserver.com/en/)(and Most other amp stacks like xampp....) is something that i coded after a friend of mine asked me if there is a fantastico installer for WAMP, which was funny since he only needed it to install WordPress. 
 
 The i figured Installing WordPress is a very simple flow of 
 
-- downloading the latest version.
-- extracting the downloaded zip file.
-- creating a new database.
+- Downloading the latest version.
+- Extracting the downloaded zip file.
+- Creating a new database.
+- Creating wp-config.php file.
 
 And since I'm such a good friend :) and I myself install WordPress locally on WAMP a few time a day then i coded this really quick script that takes care of the flow above and has a few more features.
 
@@ -16,10 +17,24 @@ And since I'm such a good friend :) and I myself install WordPress locally on WA
 Simply drop `installer.php` file in your www root directory and access it using your browser of choice.
 
 Once you are there you will see something similar to the image above, fill in the required fields and click `GO`
-the scrip will then download the latest copy of WordPress, extract the files to the directory you named and will create a new database (assuming that you have provided the needed information) and then will take you straight to where all you have to do is set you sites name, your username and pass and you have a new WordPress Installation ready to roll.
+the script will then download the latest copy of WordPress, extract the files to the directory you named and will create a new database (assuming that you have provided the needed information) and then will take you straight to where all you have to do is set you sites name, your username and pass and you have a new WordPress Installation ready to roll.
 
 ## License
 Copyright 2012, Ohad Raz <admin@bainternet.info>
+
+## Changelog:
+
+0.2
+- Changed logic for download.
+- Added a few other laguages by requests.
+- Chaged the UI a bit.
+- Added auto database name based on folder.
+- Added links for download and support.
+- Fixed some html errors.
+
+
+0.1
+- Initial release
 
 licensed under the GPL license: [http://www.gnu.org/licenses/gpl.html](http://www.gnu.org/licenses/gpl.html)
 license.txt file included.
